c59cf32843156f39cb5f9b25a5eb22e1498d08e0
Nikhil p rao <nikhilprao9066@gmail.com>
2024-11-09 20:09:32 -0500

    removed json file and data prep

--- OJ.R
+++ OJ.R
@@ -5,3 +5,20 @@ library(dplyr)
 
 str(OJ)
 ?OJ  # To understand the dataset
+
+set.seed(555)
+
+# Encode Purchase as a factor with levels 0 and 1
+OJ$Purchase <- as.numeric(OJ$Purchase == "CH")  # 1 if Purchase is CH, 0 if MM
+
+# Create a 50:50 train-test split
+trainIndex <- createDataPartition(OJ$Purchase, p = 0.5, list = FALSE)
+train_data <- OJ[trainIndex, ]
+test_data <- OJ[-trainIndex, ]
+
+# Separate features and target variable
+train_x <- as.matrix(train_data %>% select(-Purchase))
+train_y <- train_data$Purchase
+
+test_x <- as.matrix(test_data %>% select(-Purchase))
+test_y <- test_data$Purchase
