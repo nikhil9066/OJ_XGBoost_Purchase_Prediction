2768f73f5ba300596e2bf160f12d5d4964e073d5
Nikhil p rao <nikhilprao9066@gmail.com>
2024-11-09 20:47:54 -0500

    added xgboost algo

--- OJ.R
+++ OJ.R
@@ -16,9 +16,42 @@ trainIndex <- createDataPartition(OJ$Purchase, p = 0.5, list = FALSE)
 train_data <- OJ[trainIndex, ]
 test_data <- OJ[-trainIndex, ]
 
-# Separate features and target variable
-train_x <- as.matrix(train_data %>% select(-Purchase))
-train_y <- train_data$Purchase
+# Convert all factors (categorical variables) to numeric using model.matrix
+train_x <- model.matrix(Purchase ~ . - 1, data = train_data)  # Omit the intercept
+test_x <- model.matrix(Purchase ~ . - 1, data = test_data)
 
-test_x <- as.matrix(test_data %>% select(-Purchase))
+# The target variable remains the same
+train_y <- train_data$Purchase
 test_y <- test_data$Purchase
+
+# Convert to DMatrix
+dtrain <- xgb.DMatrix(data = train_x, label = train_y)
+dtest <- xgb.DMatrix(data = test_x, label = test_y)
+
+# Set parameters for XGBoost
+params <- list(
+  objective = "binary:logistic",  # Binary classification
+  eval_metric = "error",          # Classification error
+  max_depth = 6,                  # Tree depth
+  eta = 0.3,                      # Learning rate
+  nthread = 2                     # Number of threads
+)
+
+# Train the model
+xgb_model <- xgb.train(
+  params = params,
+  data = dtrain,
+  nrounds = 100,  # Number of boosting rounds
+  watchlist = list(train = dtrain, test = dtest),
+  early_stopping_rounds = 10,  # Stop early if no improvement
+  verbose = 1
+)
+
+# Make predictions on the test set
+pred_prob <- predict(xgb_model, dtest)
+
+# Convert probabilities to class labels (0 or 1)
+pred_class <- ifelse(pred_prob > 0.5, 1, 0)
+
+# Evaluate accuracy
+confusionMatrix(factor(pred_class), factor(test_y))
